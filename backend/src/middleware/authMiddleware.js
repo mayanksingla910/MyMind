082aeca2bec5fa21ce8966d0733ecb7273384174
Mayank Singla <Singlamayank10@gmail.com>
2025-08-30 23:52:28 +0530

    updated

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,9 +1,7 @@
 import jwt from "jsonwebtoken";
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
 
 export const authMiddleware = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // "Bearer <token>"
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) return res.status(401).json({ error: "No token provided" });
 
